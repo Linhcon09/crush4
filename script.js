d7acb9029a90bf92c9584c2eba19c34278910fe8
Đỗ Khánh Linh <162704884+Linhcon09@users.noreply.github.com>
2025-09-19 10:07:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
-    "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "EM bicc lỗi rồi ạa",
+    "Tha lỗi cho em nhéeeee",
+    "Đừng giận em nữa nhaaaa",
+    " Đừng buồn nữa nhoo",
+    " thương nhắm nhunnn đóoooo",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
